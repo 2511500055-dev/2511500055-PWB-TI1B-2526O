90b4f720921146b5bd953819555cc6adebd9da88
Aditya Ihsani Wijaya <2511500055@mahasiswa.atmaluhur.ac.id>
2025-11-13 00:39:14 +0700

    isset

--- pertemuan-07/post.php
+++ pertemuan-07/post.php
@@ -1,9 +1,19 @@
 <?php
   session_start();
-  $sesname = $_SESSION["nama"];
-  $sesemail = $_SESSION["email"];
-  $sespesan = $_SESSION["pesan"];
-  echo "$sesname $sesemail $sespesan";
+  $sesname = "";
+  if (isset($_SESSION["nama"]))
+     $sesname = $_SESSION["nama"];
+endif;
+
+$sesemail = "";
+  if (isset($_SESSION["email"]))
+     $sesemail = $_SESSION["email"];
+endif;
+
+$sesname = "";
+  if (isset($_SESSION["pesan"]))
+     $sespesan = $_SESSION["pesan"];
+endif;
 ?>
 <!DOCTYPE html>
 <html lang="en">

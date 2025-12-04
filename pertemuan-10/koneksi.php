db880d57cd79b63bed06ab5412ae551a6528b742
Aditya Ihsani Wijaya <2511500055@mahasiswa.atmaluhur.ac.id>
2025-12-04 15:19:09 +0700

    ngedoplikat read.php dan mengubah menjadi read_inc.php

--- pertemuan-10/koneksi.php
+++ pertemuan-10/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$host = "lokalhost";
+$host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "db_pwd2025";
@@ -9,3 +9,4 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
     die("koneksi gagal: " . mysqli_connect_error());
 }
+?>

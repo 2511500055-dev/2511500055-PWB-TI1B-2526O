9c9d265908870b9ee192b2366e64caa7118b2f83
Aditya Ihsani Wijaya <2511500055@mahasiswa.atmaluhur.ac.id>
2026-01-22 13:24:17 +0700

    menambahkan status sukses atau gagal

--- pertemuan-16/hapus_biodata_adit.php
+++ pertemuan-16/hapus_biodata_adit.php
@@ -13,6 +13,16 @@ $stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt, "i", $id);
 mysqli_stmt_execute($stmt);
 
-$_SESSION['flash_sukses'] = 'Data berhasil dihapus.';
+
+
+/* cek apakah data benar-benar terhapus */
+if (mysqli_stmt_affected_rows($stmt) <= 0) {
+  $_SESSION['flash_error'] = 'Data gagal dihapus.';
+} else {
+  $_SESSION['flash_sukses'] = 'Data berhasil dihapus.';
+}
+
+/* ================= AKHIR TAMBAHAN ================= */
+
 header("location: baca_biodata_adit.php");
 exit;
